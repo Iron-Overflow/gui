d6caed6f05cde4422c24111e67357c4ae88bfa1b
rosa <r215130@gmail.com>
2015-10-24 14:04:24 -0400

    more babysteps now with postman

--- src/js/main.js
+++ src/js/main.js
@@ -13,12 +13,24 @@
   })
 
   .run(function($http, $rootScope){
+    // $http.get("https://iron-overflow.herokuapp.com/")
+    $http.get("/src/test.json")
+    .then(function(arguments){
+      // console.log(arguments.data[0].title);
+      $rootScope.title = arguments.data[0].title;
+      $rootScope.body = arguments.data[0].inquiry;
+      $rootScope.createdAt = arguments.data[0].created_at;
+      $rootScope.author = 'author';
+
+
+    });
     $rootScope.votes = 12;
     $rootScope.numberAnswers = 45;
-    $rootScope.title = 'testing question title';
-    $rootScope.body = 'testing question body';
-    $rootScope.createdAt = '11-23-20015';
-    $rootScope.author = 'author';
+    // $rootScope.title = 'testing question title';
+    // $rootScope.body = 'testing question body';
+    // $rootScope.createdAt = '11-23-20015';
+    // $rootScope.author = 'author';
+
   })
   .run(function($http, $rootScope){
     $rootScope.answer_qty = 999;
