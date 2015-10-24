bae76730aa5fb7cc8bb085a31a21c492df9d5fc5
rosa <r215130@gmail.com>
2015-10-24 13:12:52 -0400

    baby steps angular

--- src/js/main.js
+++ src/js/main.js
@@ -12,4 +12,13 @@
       })
   })
 
+  .run(function($http, $rootScope){
+    $rootScope.votes = 12;
+    $rootScope.numberAnswers = 45;
+    $rootScope.title = 'testing question title';
+    $rootScope.body = 'testing question body';
+    $rootScope.createdAt = '11-23-20015';
+    $rootScope.author = 'author';
+  })
+
 })();
